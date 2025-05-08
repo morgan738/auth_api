230a80fc2847abd752d070983df149f360c0bf59
Morgan Hu <morgan.hu@macbookpro.mynetworksettings.com>
2025-05-08 13:34:11 -0400

    fixed sync eeror

--- index.js
+++ index.js
@@ -19,17 +19,10 @@ const seedDb = async () => {
       CREATE TABLE users(
         id UUID PRIMARY KEY,
         username VARCHAR(100) NOT NULL,
-        password VARCHAR(200) NOT NULL,
+        password VARCHAR(200) NOT NULL
       );
 
-      DROP TABLE IF EXISTS favorites CASCADE;
-      CREATE TABLE favorites(
-        id UUID PRIMARY KEY,
-        games_id INTEGER REFERENCES games(id) NOT NULL,
-        user_id UUID REFERENCES users(id) NOT NULL,
-        CONSTRAINT games_and_user_key UNIQUE(games_id, user_id)
-      );
-
+      
       DROP TABLE IF EXISTS games CASCADE;
       CREATE TABLE games(
         id SERIAL PRIMARY KEY,
@@ -38,8 +31,15 @@ const seedDb = async () => {
         price INTEGER,
         image VARCHAR(1000),
         rating INTEGER DEFAULT 1
-      );
-      
+        );
+        
+        DROP TABLE IF EXISTS favorites CASCADE;
+        CREATE TABLE favorites(
+          id UUID PRIMARY KEY,
+          games_id INTEGER REFERENCES games(id) NOT NULL,
+          user_id UUID REFERENCES users(id) NOT NULL,
+          CONSTRAINT games_and_user_key UNIQUE(games_id, user_id)
+        );
 
       INSERT INTO games (name,description, price, image, rating ) VALUES('God of War', 'Angry dad connects with son', 6000, 'https://upload.wikimedia.org/wikipedia/en/thumb/a/a7/God_of_War_4_cover.jpg/250px-God_of_War_4_cover.jpg', 5);
       INSERT INTO games (name,description, price, image, rating ) VALUES('Persona 5 Royal', 'Stealing desires', 12000, 'https://static.wikia.nocookie.net/megamitensei/images/6/6c/P5R_Key_Art.jpg/revision/latest?cb=20210329153657', 5);
@@ -79,16 +79,10 @@ const init = async()=> {
     //   CREATE TABLE users(
     //     id UUID PRIMARY KEY,
     //     username VARCHAR(100) NOT NULL,
-    //     password VARCHAR(200) NOT NULL,
-    //   );
+    //     password VARCHAR(200) NOT NULL
+    //     );
 
-    //   DROP TABLE IF EXISTS favorites CASCADE;
-    //   CREATE TABLE favorites(
-    //     id UUID PRIMARY KEY,
-    //     games_id INTEGER REFERENCES games(id) NOT NULL,
-    //     user_id UUID REFERENCES users(id) NOT NULL,
-    //     CONSTRAINT games_and_user_key UNIQUE(games_id, user_id)
-    //   );
+      
 
     //   DROP TABLE IF EXISTS games CASCADE;
     //   CREATE TABLE games(
@@ -99,15 +93,24 @@ const init = async()=> {
     //     image VARCHAR(1000),
     //     rating INTEGER DEFAULT 1
     //   );
+
+    //   DROP TABLE IF EXISTS favorites CASCADE;
+    //   CREATE TABLE favorites(
+    //     id UUID PRIMARY KEY,
+    //     games_id INTEGER REFERENCES games(id) NOT NULL,
+    //     user_id UUID REFERENCES users(id) NOT NULL,
+    //     CONSTRAINT games_and_user_key UNIQUE(games_id, user_id)
+    //   );
       
 
     //   INSERT INTO games (name,description, price, image, rating ) VALUES('God of War', 'Angry dad connects with son', 6000, 'https://upload.wikimedia.org/wikipedia/en/thumb/a/a7/God_of_War_4_cover.jpg/250px-God_of_War_4_cover.jpg', 5);
     //   INSERT INTO games (name,description, price, image, rating ) VALUES('Persona 5 Royal', 'Stealing desires', 12000, 'https://static.wikia.nocookie.net/megamitensei/images/6/6c/P5R_Key_Art.jpg/revision/latest?cb=20210329153657', 5);
     //   INSERT INTO games (name,description, price, image, rating ) VALUES('Helldivers 2', 'Spread democracy', 4000, 'https://helldivers.wiki.gg/images/5/5e/HD2_SteamLibrary-Portrait.jpg?ffffe7', 5);
     // `;
-    // /* INSERT INTO users (username, password) VALUES('morgan', '1234');
-    //   INSERT INTO users (username, password) VALUES('dylan', 'dDawg');
-    //   INSERT INTO users (username,password ) VALUES('cathy', 'cat456'); */
+    /* INSERT INTO users (username, password) VALUES('morgan', '1234');
+      INSERT INTO users (username, password) VALUES('dylan', 'dDawg');
+      INSERT INTO users (username,password ) VALUES('cathy', 'cat456'); */
+    
 
     // await client.query(SQL);
 
