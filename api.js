ecdef724d14dbfe3043efccc09a36b84878a5521
Morgan Hu <morgan.hu@macbookpro.mynetworksettings.com>
2025-11-13 09:48:47 -0500

    auth needed for post and delete game

--- api.js
+++ api.js
@@ -1,129 +1,131 @@
-const express = require('express');
+const express = require("express");
 const app = express.Router();
 const {
-    authenticate,
-    findUserByToken,
-    getAllUsers,
-    getAllGames,
-    getSingleGame,
-    addNewGame,
-    deleteGame, 
-    updateGame,
-    createUser,
-    createFavorite,
-    fetchFavorites,
-    deleteFavorite
-} = require('./db');
+  authenticate,
+  findUserByToken,
+  getAllUsers,
+  getAllGames,
+  getSingleGame,
+  addNewGame,
+  deleteGame,
+  updateGame,
+  createUser,
+  createFavorite,
+  fetchFavorites,
+  deleteFavorite,
+} = require("./db");
 
-const isLoggedIn = async(req,res,next) => {
-    try {
-      const user = await findUserByToken(req.headers.authorization)
-      req.user = user
-      console.log("loggedin ", req.user)
-      next()
-    } catch (error) {
-      next(error)
-    }
-}
-
-app.get('/users', async(req,res,next) => {
-    try {
-        res.send(await getAllUsers());
-    } catch (error) {
-        next(error)
-    }
-})
-
-app.post('/login', async(req,res,next) => {
-    try {
-      const response = await authenticate(req.body)
-      res.send({token: response})
-    } catch (error) {
-      next(error)
-    }
-})
-
-app.post('/users', async (req,res,next) => {
+const isLoggedIn = async (req, res, next) => {
   try {
-    const response = await createUser(req.body)
-    res.send(response)
+    const user = await findUserByToken(req.headers.authorization);
+    req.user = user;
+    //console.log("loggedin ", req.user)
+    next();
   } catch (error) {
-    next(error)
+    next(error);
   }
-})
+};
 
-app.get('/me', isLoggedIn, async (req,res,next) => {
-    try {
-      res.send(req.user)
-    } catch (error) {
-      next(error)
-    }
-})
+app.get("/users", async (req, res, next) => {
+  try {
+    res.send(await getAllUsers());
+  } catch (error) {
+    next(error);
+  }
+});
 
-app.get('/games', async (req,res,next) => {
+app.post("/login", async (req, res, next) => {
+  try {
+    const response = await authenticate(req.body);
+    res.send({ token: response });
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.post("/users", async (req, res, next) => {
+  try {
+    const response = await createUser(req.body);
+    res.send(response);
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.get("/me", isLoggedIn, async (req, res, next) => {
+  try {
+    res.send(req.user);
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.get("/games", async (req, res, next) => {
   try {
     res.send(await getAllGames());
   } catch (error) {
-    next(error)
+    next(error);
   }
-})
+});
 
-app.get('/games/:id', async(req,res,next) => {
+app.get("/games/:id", async (req, res, next) => {
   try {
-    res.send(await getSingleGame(req.params.id))
+    res.send(await getSingleGame(req.params.id));
   } catch (error) {
-    next(error)
+    next(error);
   }
-})
+});
 
-app.post('/games', async(req,res,next) => {
+app.post("/games", isLoggedIn, async (req, res, next) => {
   try {
-    res.send(await addNewGame(req.body))
+    res.send(await addNewGame(req.body));
   } catch (error) {
-    next(error)
+    next(error);
   }
-})
+});
 
-app.delete('/games/:id', async(req,res,next) => {
+app.delete("/games/:id", isLoggedIn, async (req, res, next) => {
   try {
-    res.send(await deleteGame(req.params.id))
+    res.send(await deleteGame(req.params.id));
   } catch (error) {
-    next(error)
+    next(error);
   }
-})
+});
 
-app.put('/games/:id', async(req,res,next) => {
+app.put("/games/:id", async (req, res, next) => {
   try {
-    res.send(await updateGame(req.body, req.params.id*1))
+    res.send(await updateGame(req.body, req.params.id * 1));
   } catch (error) {
-    next(error)
+    next(error);
   }
-})
+});
 
-app.get('/favorites', isLoggedIn, async(req, res, next)=> {
+app.get("/favorites", isLoggedIn, async (req, res, next) => {
   try {
     res.send(await fetchFavorites(req.user.id));
-  }
-  catch(ex){
+  } catch (ex) {
     next(ex);
   }
 });
 
-app.post('/favorites', isLoggedIn, async(req, res, next)=> {
+app.post("/favorites", isLoggedIn, async (req, res, next) => {
   try {
-    res.send(await createFavorite({user_id: req.user.id, games_id: req.body.games_id }));
-  }
-  catch(ex){
+    res.send(
+      await createFavorite({
+        user_id: req.user.id,
+        games_id: req.body.games_id,
+      })
+    );
+  } catch (ex) {
     next(ex);
   }
 });
 
-app.delete('/favorites/:id', isLoggedIn, async(req, res, next)=> {
+app.delete("/favorites/:id", isLoggedIn, async (req, res, next) => {
   try {
-    await deleteFavorite({ fav_id: req.params.id, user_id: req.user.id});
+    await deleteFavorite({ fav_id: req.params.id, user_id: req.user.id });
     res.sendStatus(201);
-  }
-  catch(ex){
+  } catch (ex) {
     next(ex);
   }
 });
