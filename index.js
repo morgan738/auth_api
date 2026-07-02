63382711bb680451b359f1e871bcbf3545f111d3
Morgan Hu <morgan.hu@macbookpro.mynetworksettings.com>
2026-07-02 12:39:50 -0400

    fixed broken image

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ const seedDb = async () => {
 
       INSERT INTO games (name,description, price, image, rating ) VALUES('God of War', 'Angry dad connects with son', 60, 'https://upload.wikimedia.org/wikipedia/en/thumb/a/a7/God_of_War_4_cover.jpg/250px-God_of_War_4_cover.jpg', 5);
       INSERT INTO games (name,description, price, image, rating ) VALUES('Persona 5 Royal', 'Stealing desires', 120, 'https://i.scdn.co/image/ab67616d0000b27359a6022f671f929b9979011d', 5);
-      INSERT INTO games (name,description, price, image, rating ) VALUES('Helldivers 2', 'Spread democracy', 40, 'https://helldivers.wiki.gg/images/5/5e/HD2_SteamLibrary-Portrait.jpg?ffffe7', 5);
+      INSERT INTO games (name,description, price, image, rating ) VALUES('Helldivers 2', 'Spread democracy', 40, 'https://helldivers.wiki.gg/images/thumb/Helldivers_2_Key_Art_2x3.png/800px-Helldivers_2_Key_Art_2x3.png?21106d', 5);
       INSERT INTO games (name,description, price, image, rating ) VALUES('Trails in the Sky 1st Chapter', 'Becoming the best bracer', 60, 'https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/3375780/a256c3cc1601eb983578ab77ae36c121091a3f4f/capsule_616x353.jpg?t=1777572632', 5);
       INSERT INTO games (name,description, price, image, rating ) VALUES('Hitman World Of Assassination', 'Well done, 47', 30, 'https://store-images.s-microsoft.com/image/apps.46241.13747708785346246.26b7a239-7aa3-4075-aceb-3fe6324cd11b.22bae141-a400-44a8-b4fa-9836c6e260e5', 5);
       INSERT INTO games (name,description, price, image, rating ) VALUES('Resident Evil Requiem', 'Leon OP', 70, 'https://upload.wikimedia.org/wikipedia/en/thumb/1/15/Resident_Evil_Requiem_Cover_Art.jpg/250px-Resident_Evil_Requiem_Cover_Art.jpg', 5);
